0251438b24fff2a62068aee6959006a30dd42c09
Phillip Harrington <philsown@gmail.com>
2021-09-19 19:14:57 -0400

    - Makes the schedule purdy

---
+++ _nuxt/a12addf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{344:function(t,e,n){"use strict";n.r(e);var l={},c=n(8),component=Object(c.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"speakers-list"})}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1632093160/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Feversec-ctf","\u002Foak-city-locksport","\u002Fsecure-code-warrior-tournament","\u002Fdfir-against-dark-arts"]})
