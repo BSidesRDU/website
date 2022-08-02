8252b7e7ce01618b37ab950e91bf8c760ed877e2
Phillip Harrington <philsown@gmail.com>
2022-08-02 02:05:14 -0400

    - Adds the header stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,116 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'bsidesrdu',
+        title: 'BSidesRDU',
         htmlAttrs: {
             lang: 'en',
         },
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
+            {
+                hid: 'description',
+                name: 'description',
+                content:
+                    'Security BSides is a community-driven framework for building events for and by cyber security community members. The goal is to expand the spectrum of conversation beyond the traditional confines of space and time. It creates opportunities for individuals to both present and participate in an intimate atmosphere that encourages collaboration. It is an intense event with discussions, demos, and interaction from participants. It is where conversations for the next-big-thing are happening. Security is top of mind across the entire sphere of IT and the world beyond. Therefore, more people and organizations are interested in the next new thing in security. BSides is the place where these people come to collaborate, learn and share. With many tech-companies, colleges and universities in Raleigh, Durham, Chapel Hill and surrounding areas, it is also an international center of innovation in the security industry. Security B-Sides Raleigh-Durham (B-Sides RDU) is proud to have had great speaker lineups at our events including keynotes by Dan Kaminsky, Dave Kennedy, Paul Vixie, BenTen, Jay Beale, G.Mark Hardy, and for B-Sides RDU 2017: Cliff Stoll.',
+            },
             { name: 'format-detection', content: 'telephone=no' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1, minimum-scale=1',
+            },
+            { name: 'msapplication-TileColor', content: '#ffffff' },
+            { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
+            { name: 'theme-color', content: '#ff0000' },
+            { property: 'og:url', content: 'https://bsidesrdu.org' },
+            { property: 'og:type', content: 'website' },
+            { property: 'og:title', content: 'BSidesRDU' },
+            {
+                property: 'og:description',
+                content:
+                    'Security BSides is a community-driven framework for building events for and by cyber security community members. The goal is to expand the spectrum of conversation beyond the traditional confines of space and time. It creates opportunities for individuals to both present and participate in an intimate atmosphere that encourages collaboration. It is an intense event with discussions, demos, and interaction from participants. It is where conversations for the next-big-thing are happening.',
+            },
+            {
+                property: 'og:image',
+                content: 'https://bsidesrdu.org/img/bsides-2022-logo-og.png',
+            },
+            {
+                property: 'og:image:secure_url',
+                content: 'https://bsidesrdu.org/img/bsides-2022-logo-og.png',
+            },
         ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '57x57',
+                href: '/apple-icon-57x57.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '60x60',
+                href: '/apple-icon-60x60.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '72x72',
+                href: '/apple-icon-72x72.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '76x76',
+                href: '/apple-icon-76x76.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '114x114',
+                href: '/apple-icon-114x114.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '120x120',
+                href: '/apple-icon-120x120.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '144x144',
+                href: '/apple-icon-144x144.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '152x152',
+                href: '/apple-icon-152x152.png',
+            },
+            {
+                rel: 'apple-touch-icon',
+                sizes: '180x180',
+                href: '/apple-icon-180x180.png',
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '192x192',
+                href: '/android-icon-192x192.png',
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '32x32',
+                href: '/favicon-32x32.png',
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '96x96',
+                href: '/favicon-96x96.png',
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '16x16',
+                href: '/favicon-16x16.png',
+            },
+            { rel: 'manifest', href: '/manifest.json' },
+        ],
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
