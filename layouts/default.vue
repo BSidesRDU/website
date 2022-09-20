ccd7980174549b6a0aeccbda624d767c19e7317b
Phillip Harrington <philsown@gmail.com>
2022-09-19 22:35:22 -0400

    adding menus sponsorships linkedin slack updates

--- layouts/default.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <layouts-page-header />
-    <layouts-page-main />
-    <layouts-page-footer />
-  </div>
-</template>
--- plugins/sections.js
+++
@@ -1,9 +0,0 @@
-export default function ({ $config: { sections: defaults } }, inject) {
-  inject(
-    'sections',
-    Object.keys(defaults).reduce((acc, key) => {
-      acc[key] = defaults[key] === 'true';
-      return acc;
-    }, {})
-  );
-}
